e1d610d25010d8de365e84e4a2745059e9246f31
Lukas Deutz <lukas.deutz@mailfence.com>
2020-11-28 18:06:36 -0500

    Add caching to axios requests

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,18 +1,32 @@
 import axios from "axios";
-import { PokemonURL } from "../config";
+import { setup } from "axios-cache-adapter";
+import { BaseURL, PokemonURL } from "../config";
+import localforage from "localforage";
+
+const store = localforage.createInstance({
+  driver: [localforage.INDEXEDDB, localforage.WEBSQL, localforage.LOCALSTORAGE],
+  name: "lagerfeuer-pokedex"
+});
+
+const api = setup({
+  cache: {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    store: store
+  },
+});
 
 export async function fetchAllPokemon(like) {
   const regex = like !== null ? new RegExp(like) : new RegExp("");
 
-  const initial = await axios.get(PokemonURL); // first request contains count of all pokemon
-  const _list = await axios.get(PokemonURL + `?limit=${initial.data.count}`);
+  const initial = await api.get(PokemonURL); // first request contains count of all pokemon
+  const _list = await api.get(PokemonURL + `?limit=${initial.data.count}`);
 
   const _pokemon = await Promise.all(
     _list.data.results
       .filter((e) => regex.test(e.name))
       .map(async (pokemon) => {
         try {
-          const result = await axios.get(pokemon.url);
+          const result = await api.get(pokemon.url);
           return result.data;
         } catch (e) {
           return null;
@@ -23,32 +37,40 @@ export async function fetchAllPokemon(like) {
 }
 
 export async function fetchPokemon(id) {
-  const result = await axios.get(PokemonURL + `/${id}`);
+  const result = await api.get(PokemonURL + `/${id}`);
   let pokemon = result.data;
 
-  const abilities = await Promise.all(pokemon.abilities.map(async (ability) => {
-    const tmp = await axios.get(ability.ability.url);
-    return tmp.data;
-  }));
+  const abilities = await Promise.all(
+    pokemon.abilities.map(async (ability) => {
+      const tmp = await api.get(ability.ability.url);
+      return tmp.data;
+    })
+  );
   pokemon.abilities = abilities;
 
-  const moves = await Promise.all(pokemon.moves.map(async (move) => {
-    const tmp = await axios.get(move.move.url);
-    return tmp.data;
-  }));
+  const moves = await Promise.all(
+    pokemon.moves.map(async (move) => {
+      const tmp = await api.get(move.move.url);
+      return tmp.data;
+    })
+  );
   pokemon.moves = moves;
 
-  const stats = await Promise.all(pokemon.stats.map(async (stat) => {
-    const tmp = await axios.get(stat.stat.url);
-    tmp.data.base_stat = stat.base_stat;
-    return tmp.data;
-  }));
+  const stats = await Promise.all(
+    pokemon.stats.map(async (stat) => {
+      const tmp = await api.get(stat.stat.url);
+      tmp.data.base_stat = stat.base_stat;
+      return tmp.data;
+    })
+  );
   pokemon.stats = stats;
 
-  const types = await Promise.all(pokemon.types.map(async (type) => {
-    const tmp = await axios.get(type.type.url);
-    return tmp.data;
-  }));
+  const types = await Promise.all(
+    pokemon.types.map(async (type) => {
+      const tmp = await api.get(type.type.url);
+      return tmp.data;
+    })
+  );
   pokemon.types = types;
 
   return pokemon;
